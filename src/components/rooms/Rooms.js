57d87ec3e9456067501b9d5beb3d435473b85bf8
Iskender-Garajayev <yoloten.isa@gmail.com>
2022-09-15 04:36:42 +0300

    Rooms

--- src/components/rooms/Rooms.js
+++ src/components/rooms/Rooms.js
@@ -1,19 +1,119 @@
-import React from 'react'
-import Sidebar from '../sidebar/Sidebar'
-import Navbar from '../navbar/Navbar'
+import React from "react";
+import Sidebar from "../sidebar/Sidebar";
+import Navbar from "../navbar/Navbar";
+import './rooms.css'
+import MoreVertOutlinedIcon from '@mui/icons-material/MoreVertOutlined';
+import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
+
 
 const Rooms = () => {
   return (
-    <div className='home'>
-      <Sidebar/>
+    <div className="home">
+      <Sidebar />
       <div className="homecontainer">
-        <Navbar/>
+        <Navbar />
         <div className="containet">
-          Rooms
+          <div class="card-row">
+            <div className=" card" >
+              <MoreVertOutlinedIcon className="card-menu" />
+              <span>01</span>
+              <AddOutlinedIcon className="card-add"/>
+            </div>
+            <div className=" card" >
+              <MoreVertOutlinedIcon className="card-menu" />
+              <span>02</span>
+              <AddOutlinedIcon className="card-add"/>
+            </div>
+            <div className=" card-yellow" >
+              <MoreVertOutlinedIcon className="card-menu" />
+              <span>03</span>
+              <AddOutlinedIcon className="card-add"/>
+            </div>
+            <div className=" card" >
+              <MoreVertOutlinedIcon className="card-menu" />
+              <span>04</span>
+              <AddOutlinedIcon className="card-add"/>
+            </div>
+            <div className=" card-red" >
+              <MoreVertOutlinedIcon className="card-menu" />
+              <span>05</span>
+              <AddOutlinedIcon className="card-add"/>
+            </div>
+            <div className=" card" >
+              <MoreVertOutlinedIcon className="card-menu" />
+              <span>06</span>
+              <AddOutlinedIcon className="card-add"/>
+            </div>
+          </div>
+          <div class="card-row">
+            <div className=" card-red" >
+              <MoreVertOutlinedIcon className="card-menu" />
+              <span>07</span>
+              <AddOutlinedIcon className="card-add"/>
+            </div>
+            <div className=" card" >
+              <MoreVertOutlinedIcon className="card-menu" />
+              <span>08</span>
+              <AddOutlinedIcon className="card-add"/>
+            </div>
+            <div className=" card-red" >
+              <MoreVertOutlinedIcon className="card-menu" />
+              <span>09</span>
+              <AddOutlinedIcon className="card-add"/>
+            </div>
+            <div className=" card-yellow" >
+              <MoreVertOutlinedIcon className="card-menu" />
+              <span>10</span>
+              <AddOutlinedIcon className="card-add"/>
+            </div>
+            <div className=" card-red" >
+              <MoreVertOutlinedIcon className="card-menu" />
+              <span>11</span>
+              <AddOutlinedIcon className="card-add"/>
+            </div>
+            <div className=" card" >
+              <MoreVertOutlinedIcon className="card-menu" />
+              <span>12</span>
+              <AddOutlinedIcon className="card-add"/>
+            </div>
+          </div>
+
+          <div class="card-row">
+            <div className=" card-red" >
+              <MoreVertOutlinedIcon className="card-menu" />
+              <span>13</span>
+              <AddOutlinedIcon className="card-add"/>
+            </div>
+            <div className=" card" >
+              <MoreVertOutlinedIcon className="card-menu" />
+              <span>14</span>
+              <AddOutlinedIcon className="card-add"/>
+            </div>
+            <div className=" card" >
+              <MoreVertOutlinedIcon className="card-menu" />
+              <span>15</span>
+              <AddOutlinedIcon className="card-add"/>
+            </div>
+            <div className=" card" >
+              <MoreVertOutlinedIcon className="card-menu" />
+              <span>16</span>
+              <AddOutlinedIcon className="card-add"/>
+            </div>
+            <div className=" card-yellow" >
+              <MoreVertOutlinedIcon className="card-menu" />
+              <span>17</span>
+              <AddOutlinedIcon className="card-add"/>
+            </div>
+            <div className=" card-yellow" >
+              <MoreVertOutlinedIcon className="card-menu" />
+              <span>18</span>
+              <AddOutlinedIcon className="card-add"/>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Rooms
+export default Rooms;
--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,37 +1,58 @@
-import React from 'react'
-import './sidebar.css'
-import BadgeOutlinedIcon from '@mui/icons-material/BadgeOutlined';
-import BedOutlinedIcon from '@mui/icons-material/BedOutlined';
-import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
-import Brightness3OutlinedIcon from '@mui/icons-material/Brightness3Outlined';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./sidebar.css";
+import BadgeOutlinedIcon from "@mui/icons-material/BadgeOutlined";
+import BedOutlinedIcon from "@mui/icons-material/BedOutlined";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import Brightness3OutlinedIcon from "@mui/icons-material/Brightness3Outlined";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
-    <div className='sidebar' >
+    <div className="sidebar">
       <div className="top">
         <span className="logo">Hotel</span>
       </div>
       <div className="center">
         <ul>
-          <li><Link to='/'>
-          <span>Main Page</span>
-          </Link></li>
           <li>
-            <Link to='/rooms'>
-            <BedOutlinedIcon/><span>Rooms</span>
+            <Link to="/" state={{ textDecoration: "none" }}>
+              <span>Main Page</span>
             </Link>
           </li>
-          <li><span>Accounting</span></li>
-          <li><BadgeOutlinedIcon/><span>Personals</span></li>
+          <li>
+            <Link to="/rooms" state={{ textDecoration: "none" }}>
+              <BedOutlinedIcon />
+              <span>Rooms</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/rezerv" state={{ textDecoration: "none" }}>
+              <span>Rezervation</span>
+            </Link>
+          </li>
+          <li>
+          <Link to="/accounting" state={{ textDecoration: "none" }}>
+              <span>Accounting</span>
+            </Link>
+          </li>
+          <li>
+          <Link to="/personals" state={{ textDecoration: "none" }}>
+            <BadgeOutlinedIcon />
+              <span>Personals</span>
+            </Link>
+          </li>
         </ul>
       </div>
-       <div className="bottom">
-        <div><LightModeOutlinedIcon className="light"/></div>
-        <div><Brightness3OutlinedIcon  className="dark"/></div>
-       </div>
+      <div className="bottom">
+        <div>
+          <LightModeOutlinedIcon className="light" />
+        </div>
+        <div>
+          <Brightness3OutlinedIcon className="dark" />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;

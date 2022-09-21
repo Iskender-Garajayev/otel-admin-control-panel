8da1f4f00a939fd2fc05a3f0680e33fe3adeaea0
Iskender Garajayev <100865466+Iskender-Garajayev@users.noreply.github.com>
2022-09-22 01:42:03 +0300

    update

--- src/pages/rezervation/Rezervation.js
+++ src/pages/rezervation/Rezervation.js
@@ -1,19 +1,41 @@
-import React from 'react'
-import Sidebar from '../../components/sidebar/Sidebar'
-import Navbar from '../../components/navbar/Navbar'
+import React from "react";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
+import "./rezervation.css";
 
 const Rezervation = () => {
   return (
-    <div className='home'>
-    <Sidebar/>
-    <div className="homecontainer">
-      <Navbar/>
-      <div className="containet">
-        Rezervation 
+    <div className="home">
+      <Sidebar />
+      <div className="homecontainer">
+        <Navbar />
+        <div className="containet">
+          <div className="form">
+            <div className="input-form">
+
+              <input type="text" placeholder=" ID Number" pattern="[0-9]" />
+            </div> <br />
+            <div className="input-form">
+              <input type="text" placeholder=" Name  " />
+            </div><br />
+            <div className="input-form">
+              <input type="text" placeholder=" Surname " />
+            </div><br />
+            <div className="input-form">
+              <input
+                type="tel"
+                id="phone"
+                name="phone"
+                placeholder=" Phone Number"
+                pattern="[0-9]"
+              />
+            </div>
+            <button className="button" >Submite</button>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default Rezervation
+export default Rezervation;

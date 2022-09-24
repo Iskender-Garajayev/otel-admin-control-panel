041968b3ad69607f29153fe75d7f64c7f513fbf0
Iskender Garajayev <100865466+Iskender-Garajayev@users.noreply.github.com>
2022-09-25 01:59:01 +0300

    Revert "update"

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './navbar.css'
-// import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 
 const Navbar = () => {
   return (

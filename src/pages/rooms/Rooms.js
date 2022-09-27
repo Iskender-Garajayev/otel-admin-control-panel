5b8bd679462821e36489f9c6be5ecfcccf18b10f
Iskender-G <garajayev@gmail.com>
2022-09-27 06:50:56 +0300

    update

--- src/pages/rooms/Rooms.js
+++ src/pages/rooms/Rooms.js
@@ -4,8 +4,7 @@ import Navbar from "../../components/navbar/Navbar";
 import "./rooms.css";
 import MoreVertOutlinedIcon from "@mui/icons-material/MoreVertOutlined";
 import AddOutlinedIcon from "@mui/icons-material/AddOutlined";
-import { Container, Row, Col, Card } from "react-bootstrap";
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const Rooms = () => {
   return (
@@ -14,10 +13,120 @@ const Rooms = () => {
       <div className="homecontainer">
         <Navbar />
         <div className="card-container">
-          <Container className=" align-items-center" >
+
+          <div class="card-row">
+            <div className="card" >
+              <MoreVertOutlinedIcon className="card-menu" />
+              <span>01</span>
+              <AddOutlinedIcon className="card-add"/>
+            </div>
+            <div className=" card" >
+              <MoreVertOutlinedIcon className="card-menu" />
+              <span>02</span>
+              <AddOutlinedIcon className="card-add"/>
+            </div>
+            <div className=" card-yellow" >
+              <MoreVertOutlinedIcon className="card-menu" />
+              <span>03</span>
+              <AddOutlinedIcon className="card-add"/>
+            </div>
+            <div className=" card" >
+              <MoreVertOutlinedIcon className="card-menu" />
+              <span>04</span>
+              <AddOutlinedIcon className="card-add"/>
+            </div>
+            <div className=" card-red" >
+              <MoreVertOutlinedIcon className="card-menu" />
+              <span>05</span>
+              <AddOutlinedIcon className="card-add"/>
+            </div>
+            <div className="card" >
+              <MoreVertOutlinedIcon className="card-menu" />
+              <span>06</span>
+              <AddOutlinedIcon className="card-add"/>
+            </div>
+          </div>
+
+          <div class="card-row">
+            <div className=" card-red" >
+              <MoreVertOutlinedIcon className="card-menu" />
+              <span>07</span>
+              <AddOutlinedIcon className="card-add"/>
+            </div>
+            <div className=" card" >
+              <MoreVertOutlinedIcon className="card-menu" />
+              <span>08</span>
+              <AddOutlinedIcon className="card-add"/>
+            </div>
+            <div className=" card-red" >
+              <MoreVertOutlinedIcon className="card-menu" />
+              <span>09</span>
+              <AddOutlinedIcon className="card-add"/>
+            </div>
+            <div className=" card-yellow" >
+              <MoreVertOutlinedIcon className="card-menu" />
+              <span>10</span>
+              <AddOutlinedIcon className="card-add"/>
+            </div>
+            <div className=" card-red" >
+              <MoreVertOutlinedIcon className="card-menu" />
+              <span>11</span>
+              <AddOutlinedIcon className="card-add"/>
+            </div>
+            <div className=" card" >
+              <MoreVertOutlinedIcon className="card-menu" />
+              <span>12</span>
+              <AddOutlinedIcon className="card-add"/>
+            </div>
+          </div>
+
+          <div class="card-row">
+            <div className=" card-red" >
+              <MoreVertOutlinedIcon className="card-menu" />
+              <span>13</span>
+              <AddOutlinedIcon className="card-add"/>
+            </div>
+            <div className=" card" >
+              <MoreVertOutlinedIcon className="card-menu" />
+              <span>14</span>
+              <AddOutlinedIcon className="card-add"/>
+            </div>
+            <div className=" card" >
+              <MoreVertOutlinedIcon className="card-menu" />
+              <span>15</span>
+              <AddOutlinedIcon className="card-add"/>
+            </div>
+            <div className=" card" >
+              <MoreVertOutlinedIcon className="card-menu" />
+              <span>16</span>
+              <AddOutlinedIcon className="card-add"/>
+            </div>
+            <div className=" card-yellow" >
+              <MoreVertOutlinedIcon className="card-menu" />
+              <span>17</span>
+              <AddOutlinedIcon className="card-add"/>
+            </div>
+            <div className=" card-yellow" >
+              <MoreVertOutlinedIcon className="card-menu" />
+              <span>18</span>
+              <AddOutlinedIcon className="card-add"/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Rooms;
+
+
+
+
+       {/* <Container className=" align-items-center" >
             <Row className="card-row">
               <Col>
-                <Card className=" card">
+                <Card className="card bg-danger">
                   <MoreVertOutlinedIcon className="card-menu" />
                   <span>01</span>
                   <AddOutlinedIcon className="card-add" />
@@ -151,110 +260,4 @@ const Rooms = () => {
                 </Card>
               </Col>
             </Row>
-          </Container>
-
-          {/* <div class="card-row">
-            <div className=" card" >
-              <MoreVertOutlinedIcon className="card-menu" />
-              <span>01</span>
-              <AddOutlinedIcon className="card-add"/>
-            </div>
-            <div className=" card" >
-              <MoreVertOutlinedIcon className="card-menu" />
-              <span>02</span>
-              <AddOutlinedIcon className="card-add"/>
-            </div>
-            <div className=" card-yellow" >
-              <MoreVertOutlinedIcon className="card-menu" />
-              <span>03</span>
-              <AddOutlinedIcon className="card-add"/>
-            </div>
-            <div className=" card" >
-              <MoreVertOutlinedIcon className="card-menu" />
-              <span>04</span>
-              <AddOutlinedIcon className="card-add"/>
-            </div>
-            <div className=" card-red" >
-              <MoreVertOutlinedIcon className="card-menu" />
-              <span>05</span>
-              <AddOutlinedIcon className="card-add"/>
-            </div>
-            <div className=" card" >
-              <MoreVertOutlinedIcon className="card-menu" />
-              <span>06</span>
-              <AddOutlinedIcon className="card-add"/>
-            </div>
-          </div>
-
-          <div class="card-row">
-            <div className=" card-red" >
-              <MoreVertOutlinedIcon className="card-menu" />
-              <span>07</span>
-              <AddOutlinedIcon className="card-add"/>
-            </div>
-            <div className=" card" >
-              <MoreVertOutlinedIcon className="card-menu" />
-              <span>08</span>
-              <AddOutlinedIcon className="card-add"/>
-            </div>
-            <div className=" card-red" >
-              <MoreVertOutlinedIcon className="card-menu" />
-              <span>09</span>
-              <AddOutlinedIcon className="card-add"/>
-            </div>
-            <div className=" card-yellow" >
-              <MoreVertOutlinedIcon className="card-menu" />
-              <span>10</span>
-              <AddOutlinedIcon className="card-add"/>
-            </div>
-            <div className=" card-red" >
-              <MoreVertOutlinedIcon className="card-menu" />
-              <span>11</span>
-              <AddOutlinedIcon className="card-add"/>
-            </div>
-            <div className=" card" >
-              <MoreVertOutlinedIcon className="card-menu" />
-              <span>12</span>
-              <AddOutlinedIcon className="card-add"/>
-            </div>
-          </div>
-
-          <div class="card-row">
-            <div className=" card-red" >
-              <MoreVertOutlinedIcon className="card-menu" />
-              <span>13</span>
-              <AddOutlinedIcon className="card-add"/>
-            </div>
-            <div className=" card" >
-              <MoreVertOutlinedIcon className="card-menu" />
-              <span>14</span>
-              <AddOutlinedIcon className="card-add"/>
-            </div>
-            <div className=" card" >
-              <MoreVertOutlinedIcon className="card-menu" />
-              <span>15</span>
-              <AddOutlinedIcon className="card-add"/>
-            </div>
-            <div className=" card" >
-              <MoreVertOutlinedIcon className="card-menu" />
-              <span>16</span>
-              <AddOutlinedIcon className="card-add"/>
-            </div>
-            <div className=" card-yellow" >
-              <MoreVertOutlinedIcon className="card-menu" />
-              <span>17</span>
-              <AddOutlinedIcon className="card-add"/>
-            </div>
-            <div className=" card-yellow" >
-              <MoreVertOutlinedIcon className="card-menu" />
-              <span>18</span>
-              <AddOutlinedIcon className="card-add"/>
-            </div>
-          </div> */}
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default Rooms;
+          </Container> */}
